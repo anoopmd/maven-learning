21cc360790c25a1a502c0f1b95c7269c1c6c5f4b
Anoop M D <anoop.md1421@gmail.com>
2021-03-23 19:22:23 +0530

    Learning how to work with lombok

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -13,5 +13,11 @@ public class App
         Addition addition = new Addition();
         System.out.println("1 + 1" + addition.add(1, 1));
         System.out.println(NumberUtils.toDouble("1234.5"));
+
+        // testing lombok
+        Fraction fraction = new Fraction();
+
+        fraction.setX(21);
+        System.out.println(fraction.getX());
     }
 }
---
+++ src/main/java/com/acme/Fraction.java
@@ -0,0 +1,8 @@
+package com.acme;
+
+import lombok.Data;
+
+@Data public class Fraction {
+  private int x;
+  private int y;
+}
